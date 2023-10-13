63fd08d2fd15347ae4f5bd70f6e866461c2073b5
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-10-13 15:11:15 +0200

    about page update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,6 +17,29 @@
 
     <section class="information">
 
+
+        <article class="info1"> 
+            <div class="lijn"> </div>
+            <p class="tekst1"> {aboutpage.aboutIntro.aboutInfo}</p>
+        </article>
+
+        <div class="info"> 
+            <img class="background-info" src="/Mask-group.png" alt="background">
+        </div>
+
+        <div class="story-image">
+            <img class="img2" src="{aboutpage.aboutIntro.contentImg.url}" alt="content">
+        </div>
+
+        <p class="infoAbout2"> {aboutpage.aboutIntro.aboutInfo2}</p>
+        
+        <hr color="#FFDA1D">
+
+        <div class="info2"> 
+            <img class="background-info2" src="/mask2-group.png" alt="background">
+        </div>
+    
+
     </section>
     {/each}
 
@@ -67,11 +90,25 @@
         margin-top: 1em;
     }
 
+    .info{
+        margin: 0 auto;
+        position: relative;
+        bottom: 25em;
+        z-index: -5;
+        overflow: hidden;
+    }
+
+    .background-info{
+        width: 100%;
+        height: 45em;
+        padding: 3em;
+    }
+
     p{
         padding: 1em;
         text-align: left;
         font-size: 1.5rem;
-
+        max-width: 30em;
         font-weight: bold;
         justify-content: center;
         margin: auto;
@@ -80,11 +117,18 @@
     .lijn {
         border-top: 3px var(--candelLight) solid;
         padding-top: 3em;
-
+        width: 24em;
         margin-left: auto;
         margin-right: auto;
-        
     }
+
+    .info1 {
+        align-items: center;
+        padding: 4em;
+        position: relative;
+        top: 5em;
+    }
+
 
     .info2 {
 
@@ -94,6 +138,12 @@
         width: 600;
     }
 
+
+    img.background-info2 {
+        width: 90%;
+        display: flex;
+        margin: 0 auto;
+        z-index: -5;
     }
 
     hr {
@@ -104,66 +154,23 @@
         margin-left: auto;
         margin-right: auto;
         margin-top: 3em;
-
-        
     }
 
-
-/* Mobile */
-
-/* @media only screen and (min-width: 320px) {
-
-*{
-        align-items: center;
+    .infoAbout2{
+        z-index: 10;
     }
 
-    h1, h2{
-        font-size: 7em;
-        display: inline-block;
-        transform: rotate(-6deg);
-        margin-bottom: 0.5em; 
-         margin-left: 1em;
-    } 
-
-    h1{
-        color: var(--candelLight);
-        margin-left: 1.5em;
-    }
-    h2{
-        color: white;
-        font-family: 'Kirang Haerang';
-         margin-left: 4em;
-        margin-top: 1em; 
+    .story-image{
+        overflow: hidden;
+        position: relative;
+        bottom: 20em;
     }
 
-    img{
-        display: block;
-        float: center; 
-        width: 70em;
-        margin: auto;
-        
+    .img2{
+        width: 75%;
+        display: flex;
+        margin: 0 auto;
     }
-
-   
-    p{
-        padding: 1em;
-        text-align: left;
-        font-size: 1.5rem;
-        width: 34em;
-        font-weight: bold;
-        justify-content: center;
-        margin: auto;
-    }
-
-    .info1 {
-        background-image: url("/Mask-group.png");
-        height: 800px;
-        align-items: center;
-        padding: 4em;
-<
-
-} */
-
 
 
 </style>
